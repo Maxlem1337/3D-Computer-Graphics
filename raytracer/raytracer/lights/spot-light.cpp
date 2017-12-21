258329df278d9686c1505267c6e297a56dc3b386
Maxim Lemmens <maxim.lemmens@student.ucll.be>
2017-12-21 15:51:00 +0100

    Fixed spotlights

--- raytracer/raytracer/lights/spot-light.cpp
+++ raytracer/raytracer/lights/spot-light.cpp
@@ -1,42 +1,50 @@
 #include "spot-light.h"
+#include "easylogging++.h"
+
 
 using namespace imaging;
 using namespace math;
 using namespace raytracer;
 
-
-	SpotLight::SpotLight(const Point3D& origin, const Point3D& destination, const Angle& a, const Color& color) : PointLightImplementation(origin), color(color), destination(destination), angle(a)
-	{
-	}
-
-	raytracer::LightRay SpotLight::cast_lightray_to(const math::Point3D & P) const
-	{
-		auto L = m_position;
-
-		Ray ray(L, destination);
-		
-		//A central shining direction specified by a vector v
-		auto v = ray.direction;
-
-		//P - L / |P - L|
-		Vector3D u = (P - L).normalized();
-
-		//u.dot(v) = cosine of the angle between the two vectors u and v.
-		//The spotlight only sends lights to location P if the angle between LP and v is less then angle/2
-		if (u.dot(v) >= cos(angle / 2))
+namespace
+{
+	class SpotLight : public raytracer::lights::_private_::PointLightImplementation {
+	public:
+		SpotLight(const math::Point3D& origin, const math::Point3D& destination, const math::Angle& a, const imaging::Color& color) : PointLightImplementation(origin), color(color), destination(destination), angle(a)
 		{
-			return LightRay(ray, color);
+
 		}
-		else
+	protected:
+		raytracer::LightRay cast_lightray_to(const math::Point3D& P) const override
 		{
-			return LightRay(ray, colors::black());
+			auto L = m_position;
+
+			Ray ray(L, P);
+
+			//A central shining direction specified by a vector v
+			auto v = ray.direction.normalized();
+
+			Vector3D u = (destination - L).normalized();
+
+			//u.dot(v) = cosine of the angle between the two vectors u and v.
+			//The spotlight only sends lights to location P if the angle between LP and v is less then angle/2
+			if (u.dot(v) >= cos(angle / 2))
+			{
+				return LightRay(ray, color);
+			}
+			else
+			{
+				return LightRay(ray, colors::black());
+			}
 		}
-	}
 
-
+		imaging::Color color;
+		math::Point3D destination;
+		math::Angle angle;
+	};
+}
 
 raytracer::LightSource lights::spot(const Point3D& origin, const Point3D& destination, const Angle& a, const Color& color)
 {
 	return LightSource(std::make_shared<SpotLight>(origin, destination, a, color));
 }
-
